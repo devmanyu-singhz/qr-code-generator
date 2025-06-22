c952ef9c0d35301aad26e4c751ffea4054f2082f
devmanyu-singhz <abhimanyupuru18077@gmail.com>
2025-06-22 18:57:24 +0530

    wrote some code in newproj

--- newproj.js
+++ newproj.js
@@ -1,1 +1,29 @@
-//comments
+//below is the code for this project (node.js)...still a prototype
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([{
+    message: "Enter the URL: ", 
+    name: "url"
+  }])
+  .then((answers) => {
+    console.log(answers.url);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+
+
+
+
+
+
+
